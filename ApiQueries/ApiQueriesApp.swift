a357c18d9b81b306a0848a92d5decae9ec7a1285
doguner1 <d.gunr@yaani.com>
2024-05-28 08:29:57 +0300

    Initial Commit

---
+++ ApiQueries/ApiQueriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiQueriesApp.swift
+//  ApiQueries
+//
+//  Created by Dogu on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ApiQueriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
